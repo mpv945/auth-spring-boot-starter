0e6ba805ffc132e7d22ceef7fe1ec3a06bf6a556
root <root@haijun.localdomain>
2023-08-08 23:21:53 +0800

    first commit

---
+++ src/main/java/io/github/mpv945/tools/authspringbootstarter/AuthSpringBootStarterApplication.java
@@ -0,0 +1,13 @@
+package io.github.mpv945.tools.authspringbootstarter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthSpringBootStarterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AuthSpringBootStarterApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/github/mpv945/tools/authspringbootstarter/AuthSpringBootStarterApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.mpv945.tools.authspringbootstarter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthSpringBootStarterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
